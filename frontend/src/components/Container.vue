03ec08dd0dd6c0e302119dc957cd8da7deeaf436
Arta <a.javadikouchaksarei@reply.de>
2026-07-22 07:12:49 +0200

    refactor(i18n): replace $tc with $t for translation consistency

--- frontend/src/components/Container.vue
+++ frontend/src/components/Container.vue
@@ -116,7 +116,7 @@
                 <!-- Ports -->
                 <div class="mb-4">
                     <label class="form-label">
-                        {{ $tc("port", 2) }}
+                        {{ $t("port", 2) }}
                     </label>
                     <ArrayInput name="ports" :display-name="$t('port')" placeholder="HOST:CONTAINER" />
                 </div>
@@ -124,7 +124,7 @@
                 <!-- Volumes -->
                 <div class="mb-4">
                     <label class="form-label">
-                        {{ $tc("volume", 2) }}
+                        {{ $t("volume", 2) }}
                     </label>
                     <ArrayInput name="volumes" :display-name="$t('volume')" placeholder="HOST:CONTAINER" />
                 </div>
@@ -145,7 +145,7 @@
                 <!-- Environment Variables -->
                 <div class="mb-4">
                     <label class="form-label">
-                        {{ $tc("environmentVariable", 2) }}
+                        {{ $t("environmentVariable", 2) }}
                     </label>
                     <ArrayInput name="environment" :display-name="$t('environmentVariable')" placeholder="KEY=VALUE" />
                 </div>
@@ -167,7 +167,7 @@
                 <!-- Network -->
                 <div class="mb-4">
                     <label class="form-label">
-                        {{ $tc("network", 2) }}
+                        {{ $t("network", 2) }}
                     </label>
 
                     <div v-if="networkList.length === 0 && service.networks && service.networks.length > 0" class="text-warning mb-3">
--- frontend/src/pages/DashboardHome.vue
+++ frontend/src/pages/DashboardHome.vue
@@ -38,7 +38,7 @@
                 <div class="col-md-5">
                     <!-- Agent List -->
                     <div class="shadow-box big-padding">
-                        <h4 class="mb-3">{{ $tc("siloAgent", 2) }} <span class="badge bg-warning" style="font-size: 12px;">beta</span></h4>
+                        <h4 class="mb-3">{{ $t("siloAgent", 2) }} <span class="badge bg-warning" style="font-size: 12px;">beta</span></h4>
 
                         <div v-for="(agentItem, endpoint) in $root.agentList" :key="endpoint" class="mb-3 agent">
                             <!-- Agent Status -->
